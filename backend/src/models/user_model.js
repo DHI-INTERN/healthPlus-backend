6c0b81b746a9b832b94933988304ea328e570d29
iJai007 <117366317+iJai007@users.noreply.github.com>
2023-01-04 19:43:45 +0530

    update

--- backend/src/models/user_model.js
+++ backend/src/models/user_model.js
@@ -1,20 +1,4 @@
-/*const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-    name: { type: String },
-    email: { type: String, /*unique: true*//* },
-    phone: { type: String, /*unique: true *//*},
-    password: { type: String },
-    address: { type: String, default: "" },
-    about: { type: String, default: "" },
-    type: { type: String, default: "user" },
-    gst: { type: String, default: "" },
-    addedon: { type: Date, default: Date.now }
-});
-
-const userModel = model("User", userSchema);
-
-module.exports = userModel;*/
 
 const mongoose = require('mongoose');
 const DocSchema = mongoose.Schema({
