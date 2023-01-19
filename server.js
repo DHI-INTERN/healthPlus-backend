67eee476a8469f0aa9e50b2344b629cc49afd638
Adarsh Prakash <geekguyadarsh@gmail.com>
2023-01-19 20:47:55 +0530

    bug fix

--- server.js
+++ server.js
@@ -23,13 +23,32 @@ app.get("/test", function (req, res) {
 app.use("/api/user", userRoutes);
 app.use("/api", userRoutes);
 
-// DB Connection
+// DB Connection\
 const mongoURI =
   "mongodb+srv://iJai007:gN7za480EO6HT713@cluster0.gshc7im.mongodb.net/trial2";
+// mongoose
+//   .connect(
+//     "mongodb+srv://iJ:gN7za480EO6HT713@cluster0.gshc7im.mongodb.net/trial2"
+//   )
+//   .then(function () {
+//     //NLnB2vd9UQrXL9ac
+//     console.log("DB connection established");
+//   })
+//   .catch(function (err) {
+//     console.log("error connecting to db");
+//     console.log(err);
+//   });
 
-mongoose.connect(mongoURI, () => {
-  console.log("DB CONNECTED");
-});
+try {
+  mongoose.connect("mongodb://127.0.0.1:27017/hiremedy");
+} catch (error) {
+  console.log("error");
+  handleError(error);
+}
+
+// mongoose.connect(mongoURI, () => {
+//   console.log("DB CONNECTED");
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
